b589747100c6f326a9bfc4214e4e1beb2e54e2f2
Khushhal Gupta <khushalgupta.98@gmail.com>
2024-09-24 19:52:36 +0530

    Update shopify-widget.js

--- shopify-widget.js
+++ shopify-widget.js
@@ -2,25 +2,56 @@
 window.shopicomUrl = "https://webhook.site";
 
 // Main application function
-function myAppJavaScript() {
-    console.log("Hello")
+var myAppJavaScript = function(jQuery) {
+    console.log("hello")
     // Fetch and inject the Intercom widget
-    fetch("/apps/web2chat-proxy")
-        .then(response => response.text())
-        .then(html => {
-            const parser = new DOMParser();
-            const parsedHtml = parser.parseFromString(html, "text/html");
-            const intercomWidgetContent = parsedHtml.querySelector("#web2chat-widget").textContent;
-            
-            const script = document.createElement("script");
-            script.type = "text/javascript";
-            script.id = "#web2chat-widget";
-            script.textContent = intercomWidgetContent;
-            
-            document.body.appendChild(script);
-        })
-        .catch(error => console.error('Error loading web2chat widget:', error));
+    jQuery.get("/apps/web2chat-proxy", function(response) {
+        var parser = new DOMParser();
+        var parsedHtml = parser.parseFromString(response, "text/html");
+        var web2chatWidgetContent = parsedHtml.querySelector("#web2chat-widget").textContent;
+        
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.id = "#web2chat-widget";
+        script.textContent = web2chatWidgetContent;
+        
+        jQuery("body").append(script);
+    });
+};
+
+// Check if the site uses jQuery slim version
+function isSiteUsesJQuerySlim() {
+    return jQuery.fn.jquery.indexOf("-ajax") > -1;
 }
 
-// Run the main function when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', myAppJavaScript);
+// Function to load a script
+var loadScript = function(url, callback) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    
+    if (script.readyState) {  // For old versions of IE
+        script.onreadystatechange = function() {
+            if (script.readyState === "loaded" || script.readyState === "complete") {
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {  // Other browsers
+        script.onload = function() {
+            callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+};
+
+// Check jQuery version and load if necessary
+if (typeof jQuery === "undefined" || parseFloat(jQuery.fn.jquery) < 1.7 || isSiteUsesJQuerySlim()) {
+    loadScript("//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js", function() {
+        jQuery191 = jQuery.noConflict(true);
+        myAppJavaScript(jQuery191);
+    });
+} else {
+    myAppJavaScript(jQuery);
+}
